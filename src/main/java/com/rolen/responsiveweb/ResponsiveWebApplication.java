2be10af29b62567db50198811a88da8b74e499dd
rolen <rolen@kakao.com>
2025-05-27 13:16:43 +0900

    First commit

---
+++ src/main/java/com/rolen/responsiveweb/ResponsiveWebApplication.java
@@ -0,0 +1,13 @@
+package com.rolen.responsiveweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResponsiveWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResponsiveWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rolen/responsiveweb/ResponsiveWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rolen.responsiveweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResponsiveWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
